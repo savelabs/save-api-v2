008be760e80179d7ccad96b460ab69a7d3eaedec
Vitor Daniel <vitor036daniel@gmail.com>
2022-04-02 17:57:45 -0300

    fix: vault access

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { AuthInput } from "./dto/auth.input"
 import { AuthType } from "./entities/auth.type"
 import { TokensService } from "../tokens/tokens.service"
 import { ClienteSuap } from "suap-sdk-javascript"
+import vault from "node-vault"
 
 @Injectable()
 export class AuthService {
@@ -33,6 +34,23 @@ export class AuthService {
     const refreshToken = await this.tokensService.create(user.id)
 
     const { api, site } = await cliente.obterCredenciais()
+
+    const vaultClient = vault({
+      apiVersion: "v1",
+      endpoint: "http://127.0.0.1:8200"
+    })
+
+    const result = await vaultClient.approleLogin({
+      role_id: "3eab5fb7-389a-efe1-ff7c-d1f45acaac3c",
+      secret_id: "4200379d-9c2d-52a3-c4be-a65c54def7e0"
+    })
+
+    vaultClient.token = result.auth.client_token
+
+    await vaultClient.write(`secret/suap/${user.id}`, {
+      api,
+      password: data.password
+    })
 
     return { user, token, refreshToken, cookies: site }
   }
